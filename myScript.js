bc080ec509b952e78d7a33cfee2084a8d5473777
sainya goyal <sainyagoyal0@gmail.com>
2022-02-24 00:17:55 +0530

    added cs via js

--- myScript.js
+++ myScript.js
@@ -4,6 +4,7 @@ side_menu_arr=['Recommended','Platters','Starters','Thalis','Rice bowls'];
 function side_menu(){
     for (let x in side_menu_arr){
        let item=document.createElement("div");
+       item.classList.add("main-box-child1-items");
        let node=document.createTextNode(side_menu_arr[x]);
        item.appendChild(node);
        let element=document.getElementById("side_menu_id");
