87b1c390835ce41bc0c052d56ff4115ad57e05e7
sainya goyal <sainyagoyal0@gmail.com>
2022-02-25 17:58:34 +0530

    added functionality to add items from the list to the cart

--- myScript.js
+++ myScript.js
@@ -17,6 +17,7 @@ const vegIcon="./images/veg.png";
 
 main_menu_arr=[
     {
+        id:1,
         veg:true,
         price:399,
         title:"Veg Platter",
@@ -24,6 +25,7 @@ main_menu_arr=[
         image:"./images/food.jpeg"
     },
     {
+        id:2,
         veg:false,
         price:699,
         title:"Non-Veg Platter",
@@ -31,6 +33,7 @@ main_menu_arr=[
         image:"./images/food.jpeg"
     },
     {
+        id:3,
         veg:false,
         price:699,
         title:"Non-Veg Platter",
@@ -38,6 +41,7 @@ main_menu_arr=[
         image:"./images/food.jpeg"
     },
     {
+        id:4,
         veg:true,
         price:69,
         title:"ice-cream",
@@ -45,6 +49,7 @@ main_menu_arr=[
         image:"./images/food.jpeg"
     },
     {
+        id:5,
         veg:true,
         price:99,
         title:"waffles",
@@ -52,6 +57,7 @@ main_menu_arr=[
         image:"./images/food.jpeg"
     },
     {
+        id:6,
         veg:false,
         price:799,
         title:"Bowl",
@@ -59,6 +65,7 @@ main_menu_arr=[
         image:"./images/food.jpeg"
     },
     {
+        id:7,
         veg:false,
         price:699,
         title:"Non-Veg Platter",
@@ -114,29 +121,109 @@ function main_menu(){
         });
         item7.append(item8)
 
-        let item9=$("<div></div>").addClass("add").text("ADD");
+        let item9=$("<div></div>").addClass("add");
         item7.append(item9);
         
+        let item11=$("<div></div>").addClass("decrement-main-menu").text("-");
+        item9.append(item11);
+
+        let item12=$("<div></div>").addClass("item-quantity-main-menu").text("ADD");
+        item9.append(item12);
         
-
-
-
-
-
-
-
-
-
-
-
-
+        let item13=$("<div></div>").addClass("increment-main-menu").text("+");
+        item9.append(item13);
         
-
+    
         
     }
 }
 
+const addItem = document.getElementsByClassName('increment-main-menu');
+const decrementItem = document.getElementsByClassName('decrement-main-menu');
+function itemQuantityChange(){
+   
+    for(let i=0;i<addItem.length;i++){
+        let count=0;
+        addItem[i].addEventListener('click', function() {
+            const div = this.previousElementSibling;
+            
+            let pare=addItem[i].parentElement.parentElement.previousElementSibling;
+            
+            count++;
+            div.innerText=count;
+            if(count==1){
+                let ele1=$("<div></div>").addClass("cart-item-box");
+                $(".main-box-child3").append(ele1);
+                let ele2=$("<div></div>").addClass("cart-icon-box");
+                ele1.append(ele2);
+                console.log(addItem[i].parentElement.parentElement.previousElementSibling);
+                let ele3=$('<img/>',{
+                    class:'cart-icon',
+                    src:$(pare.firstChild).attr('src'),
+                    alt:'food-image'
+                });
+                ele2.append(ele3);
+                
+                let ele4=$("<div></div>").addClass("cart-item-name").text($(pare).children().eq(1).text());
+                ele1.append(ele4);
+
+                let ele5=$("<div></div>").addClass("cart-number-box");
+                ele1.append(ele5);
+
+                let ele5_1=$("<div></div>").addClass("cart-decrement").text("-");
+                ele5.append(ele5_1);
+                let ele5_2=$("<div></div>").addClass("cart-quantity").text("1");
+                ele5.append(ele5_2);
+                let ele5_3=$("<div></div>").addClass("cart-increment").text("+");
+                ele5.append(ele5_3);
+
+                let ele6=$("<div></div>").addClass("cart-price").text($(pare).children().eq(2).text());
+                ele1.append(ele6);  
+            }
+            else{
+                const cartItem = document.getElementsByClassName('cart-increment');
+               
+                for(let j=0;j<cartItem.length;j++){
+                    let cartEle=cartItem[j].parentElement.parentElement;
+                    if($(pare).children().eq(1).text()== $(cartEle).children().eq(1).text()){
+                        let final_ele=cartItem[j].previousElementSibling;
+                
+                        $(final_ele).text(count);
+                    }
+                }
+
+            }
+            
+        })
+        decrementItem[i].addEventListener('click',function(){
+            const div=this.nextElementSibling;
+            count--;
+            count=Math.max(count,0);
+            let pare=addItem[i].parentElement.parentElement.previousElementSibling;
+            if(count<=0)
+                div.innerText='ADD';
+            else
+                div.innerText = count;
+
+                const cartItem = document.getElementsByClassName('cart-decrement');
+               
+                for(let j=0;j<cartItem.length;j++){
+                    let cartEle=cartItem[j].parentElement.parentElement;
+                    if($(pare).children().eq(1).text()== $(cartEle).children().eq(1).text()){
+                        let final_ele=cartItem[j].nextElementSibling;
+                        if(count<=0)
+                            $(final_ele).text("ADD");
+                        else
+                            $(final_ele).text(count);
+                    }
+                }
+        })
+    }
+}
+
+
 $(document).ready(function(){
     side_menu();
     main_menu();
+    itemQuantityChange();
 });
